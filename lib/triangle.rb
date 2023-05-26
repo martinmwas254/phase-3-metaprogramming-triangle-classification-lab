542cfdde6c95e0ce723359a1c3e8e86e7379eeb0
Martin <Martin@Martin>
2023-05-26 22:44:20 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,34 @@
 class Triangle
-  # write code here
+  def initialize(adj, opp, hyp)
+    @adj = adj
+    @opp = opp
+    @hyp = hyp
+    validate_triangle(adj, opp, hyp)
+  end
+
+  def kind
+    case
+    when @adj == @opp && @opp == @hyp
+      :equilateral
+    when @adj == @opp || @opp == @hyp || @adj == @hyp
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle(adj, opp, hyp)
+    sides = [adj, opp, hyp]
+    if sides.any? { |side| side <= 0 } || (adj + opp <= hyp) || (adj + hyp <= opp) || (opp + hyp <= adj)
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+    def message
+      "Invalid triangle. Please check the side lengths."
+    end
+  end
 end
